d95ff0025e65f1669c0a654cd121d944c7a1a37f
pedro <104280113+pedrrr@users.noreply.github.com>
2025-05-16 18:14:33 -0300

    Initial commits

---
+++ src/main/java/rendezvous/rendezvous/RendezvousApplication.java
@@ -0,0 +1,13 @@
+package rendezvous.rendezvous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendezvousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendezvousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rendezvous/rendezvous/RendezvousApplicationTests.java
@@ -0,0 +1,13 @@
+package rendezvous.rendezvous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendezvousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
